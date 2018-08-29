bdfd67e3fc536063f7bfc1167d72635f160c477f
Brendan Ko <brenny0707@gmail.com>
2018-08-29 11:50:13 -0400

    Add create user mutation and link test

--- test/models/link_test.rb
+++ test/models/link_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: links
+#
+#  id          :integer          not null, primary key
+#  url         :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class LinkTest < ActiveSupport::TestCase
